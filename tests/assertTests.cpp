677d2799384d867126f6156b36100c52d0c68c23
DumbTrollface <79498212+DumbTrollface@users.noreply.github.com>
2021-04-04 21:21:10 +0200

    Updated tests

--- tests/assertTests.cpp
+++ tests/assertTests.cpp
@@ -1,10 +1,17 @@
 /**
- * @author René Gyetvai
- * @date 03.04.2021
+ * @author René Gyetvai, Philipp Munz
+ * @date 04.04.2021
  **/
 
-#include <stdio.h>
-#include <string.h>
+#ifdef __cplusplus
+    #include <string>
+    #include <cmath>
+#else
+    #include <stdio.h>
+    #include <string.h>
+    #include <math.h>
+#endif
+
 
 /** Function for testing equality of integer values. 
  * It casts all input values to type 'long' to compare all sizes of integer numbers.
@@ -14,11 +21,7 @@
 static int assertLong(long given, long expected) {
     printf("Assert equal number test: ");
 
-    if (expected == given) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return expected == given;
 }
 
 /** Function for testing equality of floating-point numbers.
@@ -30,31 +33,45 @@ static int assertDouble(double given, double expected) {
     printf("Assert equal floating-point number test: ");
 
     double precision = 0.00001;
-    if (((given - precision) < expected) && ((given + precision) > expected)) {
-        return 0;
-    } else {
-        return 1;
+    return (fabs(given - expected) < precision);
+}
+
+
+#ifdef __cplusplus
+
+    /** Function for testing equality of strings.
+     * Utilizes the strcmp() function from string.h library.
+     * @param given represents the string (char array) given from the program
+     * @param expected represents the string (char array) expected from the user
+     **/
+    static int assertString(std::string given, std::string expected) {
+        printf("Assert equal strings test: ");
+
+        return given.compare(expected) == 0;
     }
-}
 
-/** Function for testing equality of strings.
- * Utilizes the strcmp() function from string.h library.
- * @param given represents the string (char array) given from the program
- * @param expected represents the string (char array) expected from the user
- **/
-static int assertString(char given[], char expected[]) {
-    printf("Assert equal strings test: ");
+# else 
 
-    return strcmp((char*) given, (char*) expected);
-}
+    /** Function for testing equality of strings.
+     * Utilizes the strcmp() function from string.h library.
+     * @param given represents the string (char array) given from the program
+     * @param expected represents the string (char array) expected from the user
+     **/
+    static int assertString(char* given, char* expected) {
+        printf("Assert equal strings test: ");
+
+        return strcmp(given, expected);
+    }
+#endif
 
 /** Function for printing out a test result.
  * @param value represents the value to select the correct message.
  **/
 static void message(int value) {
-    if (value == 0) {
+    if (value == 1) {
         printf("\033[32m Test passed! \033[m\n");
     } else {
         printf("\033[31m Test failed! \033[m\n");
     }
 }
+
